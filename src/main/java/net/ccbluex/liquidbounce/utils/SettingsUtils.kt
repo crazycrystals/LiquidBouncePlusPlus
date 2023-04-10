5d8236066621a69f5d2ef3816b8ced60a234f6e2
Crazy Crystals <66223163+crazycrystals@users.noreply.github.com>
2023-04-10 02:04:21 -0500

    Update SettingsUtils.kt

--- src/main/java/net/ccbluex/liquidbounce/utils/SettingsUtils.kt
+++ src/main/java/net/ccbluex/liquidbounce/utils/SettingsUtils.kt
@@ -1,5 +1,5 @@
 /*
- * LiquidBounce++ Hacked Client
+ * crystalBounce++ Hacked Client
  * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge.
  * https://github.com/PlusPlusMC/LiquidBouncePlusPlus/
  */
